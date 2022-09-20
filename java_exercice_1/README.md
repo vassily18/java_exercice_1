8ca16a43a4029f15dc7d3c83bec058a067c808ef
vassily18 <113984600+vassily18@users.noreply.github.com>
2022-09-20 13:00:19 +0200

    Add files via upload

---
+++ java_exercice_1/README.md
@@ -0,0 +1,1 @@
+# java_exercice_1
---
+++ java_exercice_1/src/HelloWorld.java
@@ -0,0 +1,5 @@
+public class Intro { // (1)
+    public static void main(String[] args) { // (2)
+        System.out.println("Hello " + (args.length > 0 ? args[0] : "World")); // (3)
+    }
+}
